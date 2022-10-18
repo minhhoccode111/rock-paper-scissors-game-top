5d4b7549cb4e1a5f699cedf28e07ee87d0d01c1e
minhhoccode111 <minhhoccode111@gmail.com>
2022-10-18 09:26:15 +0700

    add more features with button PLAY and fix case insensitive

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
 console.log("Hello World");
-// switch ((playerSelection, computerSelection)) {
-//   case ("rock", "paper"):
-//     string = "You Lose! Paper beats Rock!";
-//     break;
-//   case ("paper", "scissors"):
-//     string = "You Lose! Scissors beats Paper!";
-//     break;
-//   case ("scissors", "rock"):
-//     string = "You Lose! Rock beats Scissors!";
-//     break;
-//   case ("paper", "rock"):
-//     string = "You Win! Paper beats Rock!";
-//     break;
-//   case ("scissors", "paper"):
-//     string = "You Win! Scissors beats Paper!";
-//     break;
-//   case ("rock", "scissors"):
-//     string = "You Win! Rock beats Scissors!";
-//     break;
-//   default:
-//     string = "Tie Game!";
-// }
+
 const getComputerChoice = () => {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
 };
-// console.log(getComputerChoice());
 function playRound(playerSelection, computerSelection) {
   let string = "";
   let result = ["Tie Game!", "You Win!", "You Lose!"];
@@ -34,8 +12,8 @@ function playRound(playerSelection, computerSelection) {
     " Rock beats Scissors",
     " Scissors beats Paper",
   ];
-  let caseInsensitivePlayerSelection = playerSelection.toLowerCase();
-  if (playerSelection == "rock") {
+  let casePlayerSelection = playerSelection.toLowerCase();
+  if (casePlayerSelection == "rock") {
     switch (computerSelection) {
       case "rock":
         string = result[0];
@@ -47,7 +25,7 @@ function playRound(playerSelection, computerSelection) {
         string = result[1] + sentence[1];
         break;
     }
-  } else if (playerSelection == "paper") {
+  } else if (casePlayerSelection == "paper") {
     switch (computerSelection) {
       case "paper":
         string = result[0];
@@ -56,10 +34,10 @@ function playRound(playerSelection, computerSelection) {
         string = result[1] + sentence[0];
         break;
       case "scissors":
-        string = result[2] + sentence[1];
+        string = result[2] + sentence[2];
         break;
     }
-  } else if (playerSelection == "scissors") {
+  } else if (casePlayerSelection == "scissors") {
     switch (computerSelection) {
       case "scissors":
         string = result[0];
@@ -77,6 +55,12 @@ function playRound(playerSelection, computerSelection) {
   return string;
 }
 
-const playerSelection = prompt("What do you choose?");
-const computerSelection = getComputerChoice();
-alert(playRound(playerSelection, computerSelection));
+const chooseWeapon = () => {
+  const answer = prompt("What do you choose?");
+  return answer;
+};
+const play = () => {
+  const playerSelection = chooseWeapon();
+  const computerSelection = getComputerChoice();
+  alert(playRound(playerSelection, computerSelection));
+};
